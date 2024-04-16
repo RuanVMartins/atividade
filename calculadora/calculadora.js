ee765e36efd14f385e24ee4a33562263fd260a06
RuanVMartins <160753234+RuanVMartins@users.noreply.github.com>
2024-04-16 11:24:11 -0300

    Add files via upload

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,33 +1,26 @@
 function calcular() {
     var numero1 = parseInt(document.getElementById('numero1').value);
     var numero2 = parseInt(document.getElementById('numero2').value);
-    var operacao = (document.getElementById('escolha').value);
-    var text = document.querySelector('.resultado')
-
+    var operacao = document.getElementById('calculos').value;
+    var mostrarResultado = document.getElementById('mostrarResultado')
 
     switch(operacao) {
+        case 'mais':
+            resultado = numero1 + numero2;
+            break
 
-        case 'Escolha uma operação':
-            alert('Opção invalida! escolha outra coisa')
+        case 'menos':
+            resultado = numero1 - numero2;
+            break
 
-            case 'mais':
-                resultado = numero1 + numero2;
-                console.log(resultado);
-                break
-
-            case 'menos':
-                resultado = numero1 - numero2;
-                console.log(resultado);
-                break
-
-            case 'multiplicação':
-                resultado = numero1 * numero2;
-                console.log(resultado);
-                break
-            case 'divisão':
-                resultado = numero1 / numero2;
-                console.log(resultado);
-                break
+        case 'multiplicação':
+            resultado = numero1 * numero2;
+            break
+        case 'divisão':
+            resultado = numero1 / numero2;
+            break
+        default:
+            alert('Oi')
     }
-    texto.textContent = resultado;
+    mostrarResultado.innerHTML = resultado;
  }
